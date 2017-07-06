d114b222ad288c34f3326ae719c1e2fb22452514
Deepak Mahakale <deepakmahakale@gmail.com>
2017-07-06 21:19:19 +0530

    002 - Using sendgrid templates with rails (after)

--- 002-using-sendgrid-templates-with-rails/sendgrid_demo_after/app/mailers/subscription_mailer.rb
+++ 002-using-sendgrid-templates-with-rails/sendgrid_demo_after/app/mailers/subscription_mailer.rb
@@ -1,6 +1,19 @@
 class SubscriptionMailer < ApplicationMailer
   def send_subscription_email(subscription)
     @subscription = subscription
+
+    headers 'X-SMTPAPI' => {
+      'sub' => { '[email]' => [subscription.email] },
+      'filters' => {
+        'templates' => {
+          'settings' => {
+            'enable' => 1,
+            'template_id' => 'de9c0f46-3fea-4bab-bd91-dd5d06b909a4'
+          }
+        }
+      }
+    }.to_json
+
     mail(
       to: @subscription.email,
       subject: 'Thanks for subscribing!'
